a113195811241774f3d188fb248aeee2f138165b
A4konst <129418689+A4konst@users.noreply.github.com>
2023-09-21 14:34:52 -0300

    lllllllllll4

--- main.js
+++ main.js
@@ -2,9 +2,18 @@
 function tocaSomPom (){
 
     document.querySelector('#som_tecla_pom').play();
-
-
 }
 
 
 const listaDeTeclas = document.querySelectorAll(',tecla');
+
+let contador = 0;
+
+//enquanto
+while(contador < 9){
+    listaDeTeclas[0] onclick =tocaSomPom;
+
+contador= contador+1;
+console.log(contador);
+
+}
